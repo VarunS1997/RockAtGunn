c722f2da51848379774187d87ce9407ec4f46274
VarunS1997 <varun.1997.singh@gmail.com>
2016-04-10 17:57:09 -0700

    Homepage Completed

---
+++ index.php
@@ -0,0 +1,90 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>Rock at Gunn</title>
+    <link rel="stylesheet" type="text/css" href="CSS/generalCSS.css" />
+    <link rel="stylesheet" type="text/css" href="CSS/homeCSS.css" />
+</head>
+<body>
+    <div class="wrapper" id="page-header-wrapper">
+        <a href="index.php">
+            <div class="wrapper" id="rock-container">
+                <div id="logo-image-container">
+                    <img src="image-assets/rock-logo.jpg" alt="Sources of Strength"/>
+                </div>
+                <div id="logo-text-container">
+                    <div id="rock-title">Rock At Gunn</div>
+                    <div class="title" id="rock-caption">Reach out. Care. Know.</div>
+                </div>
+            </div>
+        </a>
+        <div class="wrapper" id="navigation-container">
+            <ul class="unconventional-list" id="navBar">
+                <li><a href="">Events</a></li>
+                <li><a href="">About Us</a></li>
+                <li><a href="">Contact Us</a></li>
+                <li><a href="">Members</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="wrapper" id="body-wrapper">
+        <div id="slideshow-container">
+            <div id="active-container" style="background-image: url('http://cdn.tinybuddha.com/wp-content/uploads/2015/06/Happy-Woman.jpg')">
+                <div id="slideshow-caption">
+                    <div class="title">
+                        Stuff
+                    </div>
+                    More Stuff
+                </div>
+            </div>
+            <div id="waiting-container">
+                <ul id="slide-list" class="unconventional-list">
+                    <li id="currentSlide" style="background-image: url('http://cdn.tinybuddha.com/wp-content/uploads/2015/06/Happy-Woman.jpg')"></li>
+                    <li style="background-image: url('http://bootstrapbay.com/blog/wp-content/uploads/2014/05/stocksnap-free-stock-photos1.jpg')"></li>
+                    <li style="background-image: url('http://rareadventures.co.uk/wp-content/themes/rare/images/backgrounds/Rare_Landing_Mountain.jpg')"></li>
+                    <li style="background-image: url('http://www.1075kiss.com/pics/Feeds/Articles/2016311/1445668/having-fun1.jpg')"></li>
+                    <li style="background-image: url('http://ajgpr.com/wp-content/uploads/2014/07/Group-happy-children-jumping-on-summer-meadow2.jpg')"></li>
+                </ul>
+            </div>
+        </div>
+        <div class="container">
+            <div class="text-container">
+                <p>
+                    Cheerfulness is the gift of sharing warmth and optimism. Sincere good cheer tends to engender happiness, energy, and responsiveness in those around us. This quality may spring from a spontaneous upwelling of joy and good will that cannot be contained; it may also refer to a deliberate choice to focus on what is good and right in the world.
+                </p>
+            </div>
+            <div class="image-container">
+                <img src="http://cdn.tinybuddha.com/wp-content/uploads/2015/06/Happy-Woman.jpg"/>
+            </div>
+        </div>
+        <div class="container">
+            <div class="image-container">
+                <img src="http://bootstrapbay.com/blog/wp-content/uploads/2014/05/stocksnap-free-stock-photos1.jpg"/>
+            </div>
+            <div class="text-container">
+                <p>
+                    Cheerfulness is the gift of sharing warmth and optimism. Sincere good cheer tends to engender happiness, energy, and responsiveness in those around us. This quality may spring from a spontaneous upwelling of joy and good will that cannot be contained; it may also refer to a deliberate choice to focus on what is good and right in the world.
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="wrapper" id="footer-wrapper">
+        <div id="footer">
+            <ul class="unconventional-list" id="footer-list">
+                <li><a href="">Events</a></li>
+                <li><a href="">About Us</a></li>
+                <li>
+                    <a href="index.php">Rock at Gunn <?php echo date("Y"); ?></a><br/>
+                    <a target="_blank" href="http://www.gunn.pausd.org">Henry M. Gunn High School</a>
+                </li>
+                <li><a href="">Contact Us</a></li>
+                <li><a href="">Members</a></li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
